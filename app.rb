1354e4a6e8becb27063aff015263f75626f5f6d4
Jessica Lin <jessicaa.lin@gmail.com>
2019-05-14 18:15:53 -0400

    Done.

--- app.rb
+++ app.rb
@@ -3,7 +3,13 @@ require_relative 'config/environment'
 class App < Sinatra::Base
 
   get '/food_form' do
+    # get info from user through this
     erb :food_form
+  end
+
+  post '/food' do
+    # post retrieved info here to match food_form.erb
+    "My name is #{params[:name]}, and I love #{params[:favorite_food]}"
   end
 
   # Add your post route and action below
